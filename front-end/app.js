025f0f15831b3aee3c42febb0c394df4f1d4114f
Thomas Kady <thomas.kady@gmail.com>
2022-05-15 17:46:11 -0500

    add browser wrapper functionality for API

--- front-end/app.js
+++ front-end/app.js
@@ -1,3 +1,19 @@
+String.prototype.shuffle = function () {
+    let string = this.split('');
+
+    for (let i = 0; i < string.length; i++) {
+        let j = Math.floor(Math.random() * i);
+        let temp = string[i];
+        string[i] = string[j];
+        string[j] = temp;
+    }
+    console.log(string.join(''));
+    return string.join('');
+};
+
+let zubat = 'thomas';
+zubat.shuffle();
+
 class AnimatedText {
     constructor(elementId) {
         this.id = elementId;
@@ -54,7 +70,7 @@ class AnimatedText {
     }
 
     nextTextBtnHandler() {
-        if (parseInt(this.id) === 4) {
+        if (parseInt(this.id) === 5) {
             this.completeAnimation();
             this.button.remove();
             return;
@@ -79,6 +95,22 @@ class Encounter {
         );
     }
 
+    getWildPokemonType() {
+        let type_1 = this.pokemon.type[0].type.name;
+        let type_2;
+        if (this.pokemon.type[1]) {
+            type_2 = this.pokemon.type[1].type.name;
+        }
+        let type;
+        if (type_1 && type_2) {
+            type = `${type_1}/${type_2}`;
+        } else {
+            type = type_1;
+        }
+        this.type = type;
+        return this.type;
+    }
+
     generateRandomPokemon() {
         const dexNum = Math.floor(Math.random() * 150);
         const pokemon = this.pokedex[dexNum];
@@ -87,12 +119,11 @@ class Encounter {
     }
 
     render() {
-        let pokemon = this.generateRandomPokemon();
-        console.log(pokemon);
-        console.log(pokemon.imgURL);
+        console.log(this.pokemon);
+        console.log(this.pokemon.imgURL);
         const wildPokemonField = document.getElementById('wild-pokemon');
         this.wildPokemonImg = document.createElement('img');
-        this.wildPokemonImg.src = pokemon.imgURL;
+        this.wildPokemonImg.src = this.pokemon.imgURL;
         wildPokemonField.appendChild(this.wildPokemonImg);
     }
 
@@ -100,7 +131,10 @@ class Encounter {
         if (this.wildPokemonImg) {
             this.wildPokemonImg.remove();
         }
+        this.pokemon = this.generateRandomPokemon();
         this.render();
+        this.getWildPokemonType();
+        console.log(this.type);
     }
 }
 
@@ -109,18 +143,25 @@ class PlayerSelection {
 
     constructor(pokedex) {
         this.pokedex = pokedex;
+        const parentDiv = document.getElementById('pokemon-selection');
+        this.buttons = parentDiv.querySelectorAll('button');
+        console.log(this.buttons[0].id);
+        let encounter = new Encounter(this.pokedex);
     }
 
     formStartParty() {
         const BULBASAUR = this.pokedex[0];
         const CHARMANDER = this.pokedex[3];
         const SQUIRTLE = this.pokedex[6];
-        this.pokemon.push(BULBASAUR);
-        this.pokemon.push(CHARMANDER);
-        this.pokemon.push(SQUIRTLE);
+        this.grass = BULBASAUR;
+        this.fire = CHARMANDER;
+        this.water = SQUIRTLE;
+        this.pokemon.push(this.grass);
+        this.pokemon.push(this.fire);
+        this.pokemon.push(this.water);
     }
 
-    render() {
+    renderButtons() {
         this.formStartParty();
         const parentDiv = document.getElementById('pokemon-selection');
         const buttons = parentDiv.querySelectorAll('button');
@@ -133,6 +174,11 @@ class PlayerSelection {
     }
 }
 
+class Battle {
+    constructor(pokedex) {
+        this.pokedex = pokedex;
+    }
+}
 
 class Entry {
     constructor(id, name, imgURL, type) {
@@ -144,10 +190,19 @@ class Entry {
 }
 
 class Pokedex {
-    static async createEntries(type, search) {
+    static async fetchTypes() {
+        Pokedex.types = [];
+        const data = await P.getTypesList();
+        for (let i = 0; i <= 17; i++) {
+        Pokedex.types.push(data.results[i].name);
+        }
+        return Pokedex.types;
+    }
+
+    static async createEntries() {
         Pokedex.entries = [];
         for (let i = 1; i <= 151; i++) {
-            const data = await Pokedex.fetchData(type, i);
+            const data = await P.getPokemonByName(i);
             let entry = new Entry(
                 data.id,
                 data.name,
@@ -160,19 +215,14 @@ class Pokedex {
         return Pokedex.entries;
     }
 
-    static async fetchData(type, search) {
-        const res = await fetch(`https://pokeapi.co/api/v2/${type}/${search}`);
-        const data = await res.json();
-        return data;
-    }
 }
 
 class App {
     static async init() {
-        let pokedex = await Pokedex.createEntries('pokemon');
-        let encounter = new Encounter(pokedex);
+        let pokedex = await Pokedex.createEntries();
+        let types = await Pokedex.fetchTypes();
         let starters = new PlayerSelection(pokedex);
-        starters.render();
+        starters.renderButtons();
     }
 
     static renderText() {
